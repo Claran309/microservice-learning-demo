c73f10e1748f2d91c8ffc5fa7b89a7462e529faa
Claran309 <big_fell_sans@163.com>
2026-03-05 10:31:59 +0800

    D4-etcd

--- L3-etcd/api-gateway/internal/client.go
+++ L3-etcd/api-gateway/internal/client.go
@@ -3,6 +3,10 @@ package internal
 import (
 	"microservicesDemo/L3-etcd/kitex_gen/post/postservice"
 	"microservicesDemo/L3-etcd/kitex_gen/user/userservice"
+	"time"
+
+	"github.com/cloudwego/kitex/client"
+	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
 type Clients struct {
@@ -11,12 +15,18 @@ type Clients struct {
 }
 
 func InitClients() Clients {
-	userClient, err := userservice.NewClient("user")
+	// 创建etcd解析器
+	r, err := etcd.NewEtcdResolver([]string{"http://127.0.0.1:2379"})
 	if err != nil {
 		panic(err)
 	}
 
-	postClient, err := postservice.NewClient("post")
+	userClient, err := userservice.NewClient("user-service", client.WithResolver(r), client.WithRPCTimeout(5*time.Second))
+	if err != nil {
+		panic(err)
+	}
+
+	postClient, err := postservice.NewClient("post-service", client.WithResolver(r), client.WithRPCTimeout(5*time.Second))
 	if err != nil {
 		panic(err)
 	}
